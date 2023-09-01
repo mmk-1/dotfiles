c72e3b086444b64f4ffe767420bc6e7bc6f88e40
Mahdi Khosravi <mmk1776@gmail.com>
2023-09-01 09:02:34 +0300

    set outline of setting fedora up

---
+++ setup_fedora.sh
@@ -0,0 +1,22 @@
+# Configure DNF
+# 1. /etc/dnf/dnf.conf
+# 2. add:
+#   max_parallel_downloa=5
+#   fastest_mirror=True
+#
+# sudo dnf update -y 
+#
+# Add RPM Fusion repository
+#
+# Install media codecs from RPM Fusion
+#
+# Install Neovim
+# Install Tmux
+# Install ZSH
+# Install Docker
+# Install FZF (from github)
+# Install Obsidian (flatpak)
+# Install Edge (rpm file)
+# Install Morgen
+# Install Dropbox
+# Install Zoom
