1e6dc17aa38053e4b2823730c97b91344b33cb39
Mahdi Khosravi <87431684+mmk-1@users.noreply.github.com>
2026-08-31 14:00:23 +0300

    Minor tweaks to hypr and waybar

--- private_dot_config/hypr/hyprland.lua
+++ private_dot_config/hypr/hyprland.lua
@@ -295,7 +295,6 @@ local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
 local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
-hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("~/bin/powermenu.sh"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd(menu))
